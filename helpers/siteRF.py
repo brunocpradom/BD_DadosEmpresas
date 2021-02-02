2908a120ecf53080dfc2213aa0ecc979bd759657
b <brunocpradom@gmail.com>
2021-02-02 09:00:23 -0300

    InterfaceDB created

---
+++ helpers/siteRF.py
@@ -0,0 +1,56 @@
+from dateutil.parser import parse  
+import re
+from url.request import urlopen
+import os
+#importar conexão com banco de dados
+
+
+class SiteRF():
+    
+    def __init__(self):
+        self.url ="https://receita.economia.gov.br/orientacao/tributaria/cadastros/cadastro-nacional-de-pessoas-juridicas-cnpj/dados-publicos-cnpj"
+        self.zip = '' #Colocar aqui o caminho para a pasta zip. Está em algum arquivo
+    
+    def search_download_urls(self):
+        
+        r = requests.get('https://receita.economia.gov.br/orientacao/tributaria/cadastros/cadastro-nacional-de-pessoas-juridicas-cnpj/dados-publicos-cnpj')
+        urls = re.findall(r'http://200.152.38.155/CNPJ/DADOS_ABERTOS_CNPJ_\d*.zip',r.text)
+    
+        return urls
+    
+    def download_file(self,url):
+        os.chdir(self.zip)
+        name = url.split('/')[-1]
+        download = requests.get(url,stream = True)
+        size = download.headers['Content-length']
+        pbar = tqdm(total = int(size),unit = 'B', unit_scale= True, unit_divisor = 1000, desc=url.split('/')[-1])
+    
+        with open(name, 'wb') as file:
+            for chunk in download.iter_content(chunk_size = 1000):
+                if chunk:
+                    file.write(chunk)
+                    pbar.update(1000)
+        pbar.close()
+        return size
+    
+    def downloadFilesInParalell(self):
+        print('Iniciando download de arquivos.')
+        #Com o parallel_backend consigo ver todas as barras de progresso separadamente. Se eu colocar somente o Parallel
+        #eu consigo ver a barra de progresso, mas uma de cada vez. Elas ficam se alternando na tela.
+        urls = self.search_download_urls()
+        with parallel_backend('threading',n_jobs = 4):
+            Parallel( verbose = 70)(delayed(self.download_file)(url)for url in urls)
+    
+        return True
+    
+    def check_RF_update_date(self):
+        """Essa função procura pela data de atualização no site da receita federal
+        """
+        html = urlopen(
+            """https://receita.economia.gov.br/orientacao/tributaria/cadastros/cadastro-nacional-de-pessoas-juridicas-cnpj/dados-publicos-cnpj"""
+            )
+        urls = re.findall(
+            'Data de geração do arquivo: (\d\d/\d\d/\d\d\d\d)',html.read()
+            )
+        dateRF = parse(url[0])
+        return dateRF
---
+++ models/InterfaceDB.py
@@ -0,0 +1,80 @@
+from pymongo import MongoClient
+#importar Sqlalchemy
+
+class MongoDB():
+    def __init__(self):
+        """Essa função estabele a conexão com o banco de dados. 
+        """        
+        self.db = MongoClient().dados_empresas
+        
+    def search(self, search_params_dict):
+        """Essa função recebe um dicionário com os parâmetros a serem buscados.
+        E retorna uma lista com os resultados da pesquisa
+        Ex: search_params = {'cnpj':'000000000000', 'cnae_fiscal':'0000000'}
+        
+        """
+        result = self.db.empresas.find(search_params,{'_id':0})
+        
+        return list(result)
+        
+    def insertMany(self, documents_dict):
+        """Essa função recebe um dicionário com os valores a serem inseridos
+        e os insere no banco de dados
+        """
+        try:
+            result = self.db.empresas.insert_many(documents_dict)
+            return True
+        except:
+            print('Error inserting')
+            return False
+            
+    
+    def checkUpdateDate(self):
+        result = self.db.atualizacao.find(
+                {'ultima_atualizacao':{'$regex': '\d\d/\d\d/\d\d\d\d'}},
+                {'_id':0}
+                )
+        dateDB = parse(list(result)[0]['ultima_atualizacao'])
+        return dateDB
+        
+
+class SQL():
+    
+    def __init__(self):
+        self.engine = create_engine(("postgresql://postgres:ddd@172.17.0.2/empresas1")) 
+        self.Session = sessionmaker(bind=engine)
+        self.session = Session()
+        
+        
+    def search(self,search_params_dict):
+        
+        empresa =self.session.query(Empresas).filter_by(CNPJ = cnpj)
+        
+        return list(empresa)
+        
+    def insertMany(self):
+        pass
+    
+    def checkUpdateDate(self):
+        result =self.session.query(Config).filter(
+                Config.ultima_atualizacao.like('\d\d/\d\d/\d\d\d\d')
+                )
+        #Processar as informações de result e retornar apenas a data
+        
+        return dateDB
+        
+        
+class Interface_BD():
+    """Essa classe vai ser herdada por toda classe que faça conexão com o banco
+    de dados.Caso queira trabalhar com o mongoDB, ela deve herdá-lo, caso queira 
+    trabalhar com qualquer banco suportado pelo SQLAlchemy, BancoDeDados deve
+    herdar SQL
+    """
+    def search(self):
+        super().search()
+        
+    def insertMany(self):
+        super().insert()
+        
+    def checkUpdateDate(self):
+        super().checkUpdateDate()
